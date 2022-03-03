a7e1ce32c667d270a1974659e72bf3891c633553
Ramyadevi Narayanasamy <ramyadevi.n@iqzsystems.com>
2022-03-03 17:52:42 +0530

    Success

--- Pega_RMC_Cucumber_BDD_BS_Jenkins_Chrome/src/test/java/pageObjectModel/Leads.java
+++ Pega_RMC_Cucumber_BDD_BS_Jenkins_Chrome/src/test/java/pageObjectModel/Leads.java
@@ -169,23 +169,27 @@ public class Leads {
 		try {
 			WebDriverWait wait = new WebDriverWait(ldriver, 50);
 			Thread.sleep(30000);
-			//ldriver.switchTo().frame("top_nav");
 			ldriver.switchTo().frame(ldriver.findElement(By.id("top_nav")));
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='l'][.='crm']")));
 			WebElement linkCRM = ldriver.findElement(By.xpath("//a[@class='l'][.='crm']"));
 			linkCRM.click();
 			Thread.sleep(30000);
-			ldriver.switchTo().parentFrame();
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/pfsca/SalesAutomation']")));
+			WebElement clickProspects = ldriver.findElement(By.xpath("//a[@href='/pfsca/SalesAutomation']"));
+			Thread.sleep(30000);
+			//ldriver.switchTo().parentFrame();
 		} catch (org.openqa.selenium.StaleElementReferenceException ex) {
 			WebDriverWait wait = new WebDriverWait(ldriver, 50);
 			Thread.sleep(30000);
-			//ldriver.switchTo().frame("top_nav");
 			ldriver.switchTo().frame(ldriver.findElement(By.id("top_nav")));
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='l'][.='crm']")));
 			WebElement linkCRM = ldriver.findElement(By.xpath("//a[@class='l'][.='crm']"));
 			linkCRM.click();
 			Thread.sleep(30000);
-			ldriver.switchTo().parentFrame();
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/pfsca/SalesAutomation']")));
+			WebElement clickProspects = ldriver.findElement(By.xpath("//a[@href='/pfsca/SalesAutomation']"));
+			Thread.sleep(30000);
+			//ldriver.switchTo().parentFrame();
 		}
 		
 	}
